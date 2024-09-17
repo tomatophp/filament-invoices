90b5f8048e8b111e1437ea0390d02aa1f84557e8
Fady Mondy <info@3x1.io>
2024-09-17 19:39:29 +0300

    fix ->isEnabled()

--- src/FilamentInvoicesPlugin.php
+++ src/FilamentInvoicesPlugin.php
@@ -19,10 +19,8 @@ class FilamentInvoicesPlugin implements Plugin
 
     public function register(Panel $panel): void
     {
-        if(class_exists(Module::class)){
-            if(\Nwidart\Modules\Facades\Module::find('FilamentInvoices')?->isEnabled()){
-                $this->isActive = true;
-            }
+        if(class_exists(Module::class) && \Nwidart\Modules\Facades\Module::find('FilamentInvoices')?->isEnabled()){
+            $this->isActive = true;
         }
         else {
             $this->isActive = true;
